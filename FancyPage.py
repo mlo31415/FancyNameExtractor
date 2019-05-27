dc61873626317453002ec72d1a80d1441cecfe74
mlo31415 <mlo@baskerville.org>
2019-05-26 20:54:29 -0400

    Improve comments

--- FancyPage.py
+++ FancyPage.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass
 @dataclass(order=False)
 class FancyPage:
     CanonName: str=None     # The page's Wikidot canonical name
-    Title: str=None        # The page's title
-    Redirect: str=None      # If this is a redirect page, the name of the page to which it redirects
-    UltimateRedirect: str=None  # If this is a redirect page, points to the ultimate page that this chain of redirects leads to
+    Title: str=None         # The page's title
+    Redirect: str=None      # If this is a redirect page, the non-canonical name of the page to which it redirects
+    UltimateRedirect: str=None      # If this is a redirect page, the non-canonical name of the ultimate page that this chain of redirects leads to
     Tags:   list=None       # A list of tags associated with this page
     OutgoingReferences: list=None   # A list of all the references on this page
 
